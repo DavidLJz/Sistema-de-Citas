2d837cfe8f3a49b430ef7412b77378b378755302
David LJ <4dlj1995@gmail.com>
2022-03-20 23:03:30 -0600

    agrega metodos a Request

--- app/Request.php
+++ app/Request.php
@@ -39,4 +39,40 @@ class Request
   {
     return $this->body;
   }
+
+  public function getParams(array $only=[]) :array
+  {
+    if ( !empty($this->body) && $this->givesJson() ) {
+      $params = $this->body;
+    } elseif ( $this->method === 'GET' ) {
+      $params = $_GET;
+    } elseif ( $this->method === 'POST' ){
+      $params = $_POST;
+    }
+
+    if ( empty($params) ) {
+      return [];
+    }
+
+    if ( !empty($only) ) {
+      $params = array_intersect_key($params, array_flip($only));
+    }
+
+    return $params;
+  }
+
+  public function requireValues(array $keys)
+  {
+    $params = $this->getParams();
+
+    if ( empty($params) ) {
+      Response::failBadRequest('Params not found', $keys);
+    }
+
+    foreach ($keys as $k) {
+      if ( !isset($params[$k]) ) {
+        Response::failBadRequest('Param not found', [$k]);
+      }
+    }
+  }
 }
