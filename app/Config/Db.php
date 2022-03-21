7aff88d56f617c1667f3786d33ab5c37f4aa34cc
David LJ <4dlj1995@gmail.com>
2022-03-20 23:03:20 -0600

    cambios a DB

--- app/Config/Db.php
+++ app/Config/Db.php
@@ -2,11 +2,31 @@
 
 namespace App\Config;
 
+use PDO;
+use Envms\FluentPDO\Query;
+
 class Db {
-  public static $db = [
+  protected static $query;
+
+  protected static $db = [
     'host' => 'localhost',
     'database' => 'dbcitas',
     'username' => 'dbuser',
     'password' => 'dbpass'
   ];
+
+  public static function getQuery() :Query
+  {
+    if ( empty(self::$query) ) {
+      $pdo = new PDO(
+        'mysql:host=' . self::$db['host'] . ';dbname=' . self::$db['database'],
+        self::$db['username'],
+        self::$db['password']
+      );
+
+      self::$query = new Query($pdo);
+    }
+
+    return self::$query;
+  }
 }
